c6cac5752c9d1f40bce044ef65608f41a73b5d71
JaeHong KIM <fkjh77777@gmail.com>
2024-02-01 19:00:29 +0900

    넘버패드 업데이트

--- Assets/PadTextInput.cs
+++ Assets/PadTextInput.cs
@@ -5,42 +5,52 @@ using UnityEngine.UI;
 
 public class PadTextInput : MonoBehaviour
 {
-    public InputField inputField;   // 숫자 값이 입력되는 InputField
-    public Text stackText;          // 스택을 표시하는 Text
+    public Text stackText;
+    public int[] sNum = new int[4];
+    public int stackIndex = 0;
+    public string stackTextString = "";
 
-    private Stack<int> valueStack = new Stack<int>();  // 입력된 숫자 값들을 저장할 스택
 
-    void Start()
+    void Update()
     {
-        // InputField에 값이 입력될 때마다 이벤트를 등록
-        inputField.onValueChanged.AddListener(OnInputValueChanged);
+        
+        
     }
 
-    void OnInputValueChanged(string newValue)
+
+    public void Push(int value)
     {
-        // InputField에서 값이 변경될 때 호출되는 함수
-
-        // 값이 숫자로 변환 가능한지 확인하고 스택에 추가
-        if (int.TryParse(newValue, out int numericValue))
+        if (stackIndex < sNum.Length)
         {
-            valueStack.Push(numericValue);
+            sNum[stackIndex] = value;
+            stackTextString += sNum[stackIndex];
 
-            // 스택의 내용을 텍스트로 업데이트
-            UpdateStackText();
+            stackIndex++;
+
+            
+                
+            
+            stackText.text = stackTextString;
+
+
+
         }
-
-        // 입력 필드 초기화
-        inputField.text = "";
+        if (stackIndex == sNum.Length)
+        {
+            ResetStack();
+        }
     }
 
-    void UpdateStackText()
+    void ResetStack()
     {
-        // 스택의 내용을 텍스트로 업데이트
-        stackText.text = "Stack:\n";
-
-        foreach (int value in valueStack)
+        stackIndex = 0; // 인덱스 초기화
+        for (int i = 0; i < sNum.Length; i++)
         {
-            stackText.text += value + "\n";
+            sNum[i] = 0; // 각 요소를 0으로 초기화
         }
+
+        stackTextString = "";
+        stackText.text = stackTextString;
+        Debug.Log("Stack reset!");
     }
 }
